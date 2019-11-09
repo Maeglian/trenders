c7d013ebb1b3080ba9475d31caed68165b67ae79
Oleg Pelipenko <oleg-fry@yandex-team.ru>
2019-11-09 15:19:25 +0300

    Merge pull request #29 in SHRI-2019/trenders from backend.ports to master
    Squashed commit of the following:
    
    commit 5c5e7a8b07c05634784069f37bff4f552fac548a
    Author: oleg.fry <oleg.fry@gmail.com>
    Date:   Sat Nov 9 15:17:44 2019 +0300
    
        default ports

--- backend/web/app.py
+++ backend/web/app.py
@@ -5,15 +5,27 @@ from trends.collectors.efir import EfirCollector
 from trends.collectors.google import GoogleCollector
 from trends.models.trends_repo import Repository
 
+
+def get_environ_or_default(env, default):
+    try:
+        value = os.environ[env]
+        if value == '':
+            return default
+        return value
+    except Exception as e:
+        return default
+
+
 if __name__ == '__main__':
     # export DATABASE_URL=postgresql://me:hackme@0.0.0.0/trends
-    print('DATABASE_URL', os.environ['DATABASE_URL'])
+    db_url = get_environ_or_default('DATABASE_URL', 'postgresql://me:hackme@0.0.0.0/trends')
+    print('DATABASE_URL', db_url)
 
-    app = create_app(os.environ['DATABASE_URL'])
+    app = create_app(db_url)
 
     repo = Repository(app.db)
-    collectors = [EfirCollector(repo, os.environ['EFIR_URL']),
-                  GoogleCollector(repo, os.environ['GOOGLE_URL'])]
+    collectors = [EfirCollector(repo, get_environ_or_default('EFIR_URL', 'http://localhost:8081')),
+                  GoogleCollector(repo, get_environ_or_default('GOOGLE_URL', 'http://localhost:8082'))]
     for c in collectors:
         c.start()
 
