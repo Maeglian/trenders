36f6745ce7bd46227a4ccf940b12ce2b7478fb46
Aleksandr Pavlovich <alexpavlovich@yandex-team.ru>
2019-11-09 03:56:21 +0300

    Merge pull request #16 in SHRI-2019/trenders from frontend.ci to master
    Squashed commit of the following:
    
    commit 7d1d14c541a197eee2891fab07b3f80e123d2b29
    Author: alexpavlovich <alexpavlovich@yandex-team.ru>
    Date:   Sat Nov 9 03:55:59 2019 +0300
    
        switch test branch to master
    
    commit 5acdbfbae08dc2a01e44c558c15c5074481568fd
    Author: alexpavlovich <alexpavlovich@yandex-team.ru>
    Date:   Sat Nov 9 03:25:51 2019 +0300
    
        ci

---
+++ client/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App started on port ${port}`));
