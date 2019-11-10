f67ba5bbd57a201891561934f4404f179fa8295f
Aleksandra Smirnova <smirnova-a-yu@yandex-team.ru>
2019-11-10 19:07:51 +0300

    Merge pull request #42 in SHRI-2019/trenders from frontend.express_route to master
    Squashed commit of the following:
    
    commit e49fae8abfa8ea8cade0fcf56d3bcebacdb056fa
    Author: Alexander Brichencov <brichencov@yandex.ru>
    Date:   Sat Nov 9 19:59:00 2019 +0300
    
        add express route for fix react routing

--- client/index.js
+++ client/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static('build'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build/index.html'))
+})
 
 const port = process.env.PORT || 3000;
 
